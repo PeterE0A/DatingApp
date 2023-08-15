2cf59670cf08082750e2fa73e9cacfd4487f0f52
Afrina Rahaman <afri0039@elev.tec.dk>
2023-08-15 17:59:09 +0200

    Add project files.

---
+++ DatingApp/Services/IDatabaseService.cs
@@ -0,0 +1,15 @@
+﻿namespace DatingApp.Services
+{
+    public interface IDatabaseService
+    {
+        bool CreateAccount(string username, string passwordHash);
+        bool DeleteAccount(int userId);
+        bool CreateProfile(int userId, string fullName, DateTime birthday, string gender, string city, string postalCode);
+        bool DeleteProfile(int userId);
+        bool Login(string username, string passwordHash);
+        bool Logout();
+        bool AddLike(int likerUserId, int likedUserId);
+        bool CheckForMatch(int userId1, int userId2);
+        // Add other methods for search, messaging, etc.
+    }
+}
