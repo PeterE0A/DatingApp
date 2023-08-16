5cc6515b6f8e71bff649f19839f49f22adb8e985
Afrina Rahaman <afri0039@elev.tec.dk>
2023-08-16 21:21:25 +0200

    might change

--- DatingApp/Services/IDatingAppService.cs
+++ DatingApp/Services/IDatingAppService.cs
@@ -4,7 +4,7 @@ namespace DatingApp.Services
 {
     public interface IDatingAppService
     {
-        Task<bool> CreateAccountAsync(string username, string password);
+        Task<bool> CreateAccountAsync(string? username, string? password);
         Task<bool> DeleteAccountAsync(int userId);
         Task<bool> CreateProfileAsync(int userId, string fullName, DateTime birthday, string gender, string city, string postalCode);
         Task<bool> DeleteProfileAsync(int userId);
